2b79561cd6dddddf88c1868a2ad236dc4c1284d1
Gavin Rouleau <gjrcmb@yahoo.com>
2014-07-27 21:36:54 -0600

    Update Savings Account

--- app/assets/javascripts/dateselector.js
+++ app/assets/javascripts/dateselector.js
@@ -2,7 +2,7 @@
 // See http://api.jqueryui.com/datepicker
 $(document).ready(function() {
     if ($("[id$=category_savings_goal_date]").length) {
-        $.getScript('../assets/jquery-ui', function() {
+        $.getScript('../assets/jquery-ui/datepicker.js', function() {
             $("[id$=category_savings_goal_date]").datepicker( {
                 dateFormat: 'm/d/yy',
                 changeMonth: true,
